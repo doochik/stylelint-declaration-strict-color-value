7a86ce3b7dd4bf22eacddf1c68a3189b2bd4244d
Aleksei Androsov <doochik@ya.ru>
2018-07-17 18:35:58 +0300

    Add tests for hex and rgb/rgba

--- test/whitelist.js
+++ test/whitelist.js
@@ -6,8 +6,7 @@ const rule = require('../index').find(rule => rule.ruleName === 'declaration-str
 testRule(rule.rule, {
     ruleName: rule.ruleName,
     config: [
-        'var(--red)',
-        'var(--green)',
+        [ 'var(--red)', 'var(--green)' ],
     ],
 
     accept: [
@@ -27,6 +26,62 @@ testRule(rule.rule, {
         {
             code: '.foo{ border: 1px solid #0f0 }',
             message: 'Unexpected color "#0f0" for property "border" (' + rule.ruleName + ')',
+            line: 1,
+            column: 7,
+        },
+    ],
+});
+
+// #hex check
+testRule(rule.rule, {
+    ruleName: rule.ruleName,
+    config: [
+        [ '#f00', '#0f0' ],
+    ],
+
+    accept: [
+        { code: '.foo{ color: #f00 }' },
+        { code: '.foo{ border: 1px solid #0f0 }' },
+    ],
+
+    reject: [
+        {
+            code: '.foo{ color: #00f }',
+            message: 'Unexpected color "#00f" for property "color" (' + rule.ruleName + ')',
+            line: 1,
+            column: 7,
+        },
+        {
+            code: '.foo{ border: 1px solid #ccc }',
+            message: 'Unexpected color "#ccc" for property "border" (' + rule.ruleName + ')',
+            line: 1,
+            column: 7,
+        },
+    ],
+});
+
+// rgb/rgba check
+testRule(rule.rule, {
+    ruleName: rule.ruleName,
+    config: [
+        [ 'rgba(255, 0, 0)', 'rgba(0, 0, 255, 0.5)' ],
+    ],
+
+    accept: [
+        { code: '.foo{ color: rgba(255, 0, 0) }' },
+        { code: '.foo{ border: 1px solid rgba(0, 0, 255, 0.5) }' },
+    ],
+
+    reject: [
+        {
+            code: '.foo{ color: #00f }',
+            message: 'Unexpected color "#00f" for property "color" (' + rule.ruleName + ')',
+            line: 1,
+            column: 7,
+        },
+        {
+            code: '.foo{ border: 1px solid #ccc }',
+            message: 'Unexpected color "#ccc" for property "border" (' + rule.ruleName + ')',
             line: 1,
             column: 7,
         },
